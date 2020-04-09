b2af93cdda940e1f3d122be5d3d5cf3b327cba6c
Fischer, Robert <robert.fischer@empa.ch>
2020-04-09 14:43:03 +0200

    added peak differences measurement (test4)

--- post_processing/13_peak_to_peak_distance.py
+++ post_processing/13_peak_to_peak_distance.py
@@ -29,7 +29,7 @@ def peak_diff_calc(file):
     tension = dyn_data.attrs['tension']
     label_matrix = dyn_data['label_matrix'].data
     labels = dyn_data['label'].data
-    throat_params = robpylib.CommonFunctions.pore_network.extract_throat_list(label_matrix, labels)
+    throats, params = robpylib.CommonFunctions.pore_network.extract_throat_list(label_matrix, np.unique(label_matrix)[1:])
     time = dyn_data['time'].data
     peak_distances = -np.ones([len(dyn_data['label'].data),20])
     heights = peak_distances.copy()
@@ -43,22 +43,41 @@ def peak_diff_calc(file):
         pore_heights.append(props['peak_heights'])
         diffs = np.diff(time[peaks])
         peak_distances[i,:len(diffs)] = diffs
-    links = np.uint16(throat_params[:,:1])
+    links = throats[:,:2].astype(np.uint16)
     link_diff = np.zeros(len(links))
     link_diff_of_max = link_diff.copy()
     # compare first peaks and max peaks
     for c in range(len(links)):
         p1 = links[c,0]
         p2 = links[c,1]
+        
+        if not p1 in labels:
+            link_diff[c] = np.NaN
+            link_diff_of_max[c] = np.NaN
+            continue  
+        if not p2 in labels:
+            link_diff[c] = np.NaN
+            link_diff_of_max[c] = np.NaN
+            continue  
         i1 = np.where(labels==p1)[0][0]
         i2 = np.where(labels==p2)[0][0]
+        if not len(pore_peaks[i1])>0: 
+            link_diff[c] = np.NaN
+            link_diff_of_max[c] = np.NaN
+            continue
+        if not len(pore_peaks[i2])>0: 
+            link_diff[c] = np.NaN
+            link_diff_of_max[c] = np.NaN           
+            continue
         ts1 = pore_peaks[i1][0]
         ts2 = pore_peaks[i2][0]
         link_diff[c] = time[ts1] - time[ts2]
         mx1 = pore_peaks[i1][np.argmax(pore_heights[i1])]
         mx2 = pore_peaks[i2][np.argmax(pore_heights[i2])]
         link_diff_of_max[c] = time[mx1] - time[mx2]
-    pore_diffs = np.concatenate((link_diff, link_diff_of_max), axis = 1)
+    pore_diffs = np.zeros((len(links),2))
+    pore_diffs[:,0] = link_diff
+    pore_diffs[:,1] = link_diff_of_max
     
     return peak_distances, sample_name, tension, heights, pore_diffs
 
@@ -67,28 +86,34 @@ c=0
 files_crude = os.listdir(data_path)
 files = []
 for file in files_crude:
-    if c>1: continue
+    # if c>1: continue
     if file[:3] == 'dyn': 
         files.append(os.path.join(data_path, file))
         c=c+1
 
 results = Parallel(n_jobs = num_cores)(delayed(peak_diff_calc)(file) for file in files)
+# results = []
+# for file in files:
+#     results.append(peak_diff_calc(file))
 
-data = np.array([])
-data_peaks = np.array([])
+test = np.zeros((1,20))
+test2 = np.zeros((1,2))
+
+data = test
+data_peaks = test
 names = []
 tensions = []
-data_025 = np.array([])
-data_100 = np.array([])
-data_300 = np.array([])
-data_025_peaks = np.array([])
-data_100_peaks = np.array([])
-data_300_peaks = np.array([])
+data_025 = test
+data_100 = test
+data_300 = test
+data_025_peaks = test
+data_100_peaks = test
+data_300_peaks = test
 
-diffs = np.array([])
-diffs_025 = np.array([])
-diffs_100 = np.array([])
-diffs_300 = np.array([])
+diffs = test2
+diffs_025 = test2
+diffs_100 = test2
+diffs_300 = test2
 
 for result in results:
     data_res = np.array(result[0])
@@ -116,20 +141,20 @@ for result in results:
 
 dataset = {'tensions': tensions, 
            'names': names,
-           'data': data,
-           'data_heights': data_peaks,
-           'diffs': diffs,
-           'data_025': data_025,
-           'data_100': data_100,
-           'data_300': data_300,
-           'data_025_heights': data_025_peaks,
-           'data_100_heights': data_100_peaks,
-           'data_300_heights': data_300_peaks,
-           'diffs_025': diffs_025,
-           'diffs_100': diffs_100,
-           'diffs_300': diffs_300}
+           'data': data[1:,:],
+           'data_heights': data_peaks[1:,:],
+           'diffs': diffs[1:,:],
+           'data_025': data_025[1:,:],
+           'data_100': data_100[1:,:],
+           'data_300': data_300[1:,:],
+           'data_025_heights': data_025_peaks[1:,:],
+           'data_100_heights': data_100_peaks[1:,:],
+           'data_300_heights': data_300_peaks[1:,:],
+           'diffs_025': diffs_025[1:,:],
+           'diffs_100': diffs_100[1:,:],
+           'diffs_300': diffs_300[1:,:]}
 
-np.save(dataset, os.path.join(data_path, 'peak_diffs.npy'))
+np.save(os.path.join(data_path, 'peak_diffs.npy'), dataset)
 
 dataset = xr.Dataset(dataset)
 
