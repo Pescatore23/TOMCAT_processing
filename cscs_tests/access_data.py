1b19a117d610bb99ed22205eb561f0d8ba2e21de
Fischer, Robert <robert.fischer@empa.ch>
2020-04-15 09:14:06 +0200

    say hello to cscs

---
+++ cscs_tests/access_data.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 08:46:37 2020
+
+@author: firo
+"""
+
+import os
+
+
+sourceFolder = r'/scratch/snx3000/fischer/pescatore'
+
+destination = os.path.join(sourceFolder, 'test')
+
+if not os.path.exists(destination):
+    os.mkdir(destination)
+    
+content = os.listdir(os.path.join(sourceFolder,'fiber_data', 'fiber_data'))
+
+regfile=os.path.join(destination,'register_log.txt')
+reglog=open(regfile,"w")
+for item in content:
+    reglog.write(item+'\n')
+reglog.close() 
