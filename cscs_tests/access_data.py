924e73f92ced359ee4474ba952fc57b21ab8dfa2
Fischer, Robert <robert.fischer@empa.ch>
2020-04-15 09:44:53 +0200

    say hello to cscs bug fix

--- cscs_tests/access_data.py
+++ cscs_tests/access_data.py
@@ -9,16 +9,18 @@ import os
 
 
 sourceFolder = r'/scratch/snx3000/fischer/pescatore'
+sourceFolder = r"W:\Robert_TOMCAT_3_netcdf4_archives"
 
 destination = os.path.join(sourceFolder, 'test')
 
 if not os.path.exists(destination):
     os.mkdir(destination)
     
-content = os.listdir(os.path.join(sourceFolder,'fiber_data', 'fiber_data'))
+content = os.listdir(os.path.join(sourceFolder,'fiber_data'))#, 'fiber_data'))
 
 regfile=os.path.join(destination,'register_log.txt')
 reglog=open(regfile,"w")
 for item in content:
+    print(item)
     reglog.write(item+'\n')
 reglog.close() 
