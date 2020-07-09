9297fe2975e761bed45f38c7e29c0408b0f4a05a
Fischer, Robert <robert.fischer@empa.ch>
2020-07-09 16:36:16 +0200

    keep exp dyn data

--- post_processing/expand_pore_space.py
+++ post_processing/expand_pore_space.py
@@ -69,6 +69,7 @@ for i in range(size):
 
 data['transition_matrix'] = None
 data['transition_2_matrix'] = None
+data['label_matrix'] = None
 data['x'] = np.arange(new_label.shape[0])
 data['y'] = np.arange(new_label.shape[1])
 data['z'] = np.arange(new_label.shape[2])
