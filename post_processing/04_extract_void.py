3d33c011d0616f961e351b1d6e46a1c05d1bc4ae
Fischer, Robert <robert.fischer@empa.ch>
2020-08-14 13:51:34 +0200

    new version for interlace voids

--- post_processing/04_extract_void.py
+++ post_processing/04_extract_void.py
@@ -88,7 +88,7 @@ for sample in os.listdir(baseFolder):
 #    if not sample == 'T4_300_5_III': continue
     # if not sample[1] == '4': continue
 #    if not sample == 'T3_025_9_III': continue
-    if not sample in robpylib.TOMCAT.INFO.samples_to_repeat: continue
+    # if not sample in robpylib.TOMCAT.INFO.samples_to_repeat: continue
     if sample == 'T4_025_2_II': continue
     if sample == 'T3_025_1': continue
     if sample in excluded_samples:
