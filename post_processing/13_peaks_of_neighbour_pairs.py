2a8adcdeb5f19eec51bd589e01d3bdfac0164a39
Robert Fischer <robert.fischer@empa.ch>
2021-06-08 13:41:06 +0200

    try poeak diff on hades

--- post_processing/13_peaks_of_neighbour_pairs.py
+++ post_processing/13_peaks_of_neighbour_pairs.py
@@ -18,13 +18,13 @@ from scipy.interpolate import interp1d
 num_cores = 8
 
 # drive = r'\\152.88.86.87\data118'
-# drive = r"NAS"
+drive = r"NAS2"
 # drive =  r"Z:\"
 # data_path = os.path.join(drive, 'Robert_TOMCAT_3_netcdf4_archives')
-# data_path = os.path.join(drive, 'Robert_TOMCAT_3b_netcdf4') #TODO: change also peak definition fot T4!!
+data_path = os.path.join(drive, 'Robert_TOMCAT_5_netcdf4') #TODO: change also peak definition fot T4!!
 # data_path = r'Z:\Robert_TOMCAT_3b_netcdf4'
 # data_path = r'A:\Robert_TOMCAT_4_netcdf4_split_v2'
-data_path = r'B:\Robert_TOMCAT_5_netcdf4'
+# data_path = r'B:\Robert_TOMCAT_5_netcdf4'
 # processing_version = 'processed_1200_dry_seg_aniso_sep'
 # processing_version = 'for_PNM'
 # folder = r'Z:\Robert_TOMCAT_3'
