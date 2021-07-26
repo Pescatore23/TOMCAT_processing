b86f3e41ffb85450574b0e0ef878eedd8d8be95b
Robert Fischer <robert.fischer@empa.ch>
2021-07-26 17:19:31 +0200

    use cpu because gpu has too little RAM

--- post_processing/22_moisture_profiles.py
+++ post_processing/22_moisture_profiles.py
@@ -6,7 +6,6 @@ Created on Mon Jul 26 16:33:00 2021
 """
 
 import h5py
-import cupy as cp
 import os
 import xarray as xr
 import numpy as np
@@ -28,23 +27,23 @@ for file in file_list:
     if file[:3] == 'dyn':
         datafile = h5py.File(os.path.join(baseFolder, file))
         time = np.array(datafile['time'])
-        transitions = cp.array(datafile['transition_matrix'])
-        time_steps = cp.unique(transitions)[1:]
+        transitions = np.array(datafile['transition_matrix'])
+        time_steps = np.unique(transitions)[1:]
         
         samples.append(datafile.attrs['name'].decode())
         
-        sample_profiles = cp.zeros((2016,271))
+        sample_profiles = np.zeros((2016,271))
         any_water = transitions>0
         
         for ts in range(1,271):
             if ts in time_steps:
-                water = cp.bitwise_and(any_water,(transitions<ts+1))
-                profile = cp.count_nonzero(water, axis = (0,1))
+                water = np.bitwise_and(any_water,(transitions<ts+1))
+                profile = np.count_nonzero(water, axis = (0,1))
                 sample_profiles[:,ts] = profile
             else:
                 sample_profiles[:,ts] = sample_profiles[:,ts-1]
         
-        profiles[:,:,cc] = cp.asnumpy(sample_profiles)
+        profiles[:,:,cc] = np.asnumpy(sample_profiles)
         cc = cc + 1
 
 data = xr.Dataset({'moisture_profile': (['height', 'time', 'sample'], profiles*vx)},
