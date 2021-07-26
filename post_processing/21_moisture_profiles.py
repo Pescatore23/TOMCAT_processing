f0c013300cce125f2c3f82b63c12e0dd8c9bed36
Robert Fischer <robert.fischer@empa.ch>
2021-07-26 17:12:18 +0200

    calculate moisture profiles inj interlaces

---
+++ post_processing/21_moisture_profiles.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 16:33:00 2021
+
+@author: firo
+"""
+
+import h5py
+import cupy as cp
+import os
+import xarray as xr
+import numpy as np
+
+px = 2.75E-6
+vx = px**3
+
+baseFolder = '/NAS/Robert_TOMCAT_4_netcdf4_split_v2'
+
+file_list = os.listdir(baseFolder)
+
+height = px*(2016-np.arange(2016))
+
+profiles = np.zeros((2016,271,12))
+samples = []
+
+cc = 0
+for file in file_list:
+    if file[:3] == 'dyn':
+        datafile = h5py.datafile(os.path.join(baseFolder, file))
+        time = np.array(datafile['time'])
+        transitions = cp.array(datafile['transition_matrix'])
+        time_steps = cp.unique(transitions)[1:]
+        
+        samples.append(datafile.attrs['name'].decode())
+        
+        sample_profiles = cp.zeros((2016,271))
+        any_water = transitions>0
+        
+        for ts in range(1,271):
+            if ts in time_steps:
+                water = cp.bitwise_and(any_water,(transitions<ts+1))
+                profile = cp.count_nonzero(water, axis = (0,1))
+                sample_profiles[:,ts] = profile
+            else:
+                sample_profiles[:,ts] = sample_profiles[:,ts-1]
+        
+        profiles[:,:,cc] = cp.asnumpy(sample_profiles)
+        cc = cc + 1
+
+data = xr.Dataset({'moisture_profile': (['height', 'time', 'sample'], profiles*vx)},
+                  coord = {'height': height,
+                           'time': time,
+                           'sample': samples})
+
+data['height'].attrs['units'] = 'm'
+data['time'].attrs['units'] = 's'
+
+destination = os.path.join(baseFolder, 'moisture_profiles.nc')
+data.to_netcdf(destination)
+        
+        
