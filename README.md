12bdb2eb6c237cbf087b41bb74a36637018315d7
Fischer Robert <robert.fischer@psi.ch>
2024-03-20 10:52:32 +0100

    added reference

--- README.md
+++ README.md
@@ -10,3 +10,48 @@ requires my Python library robpylib Pescatore23/robpylib to run, mostly for imag
 ## Usage
 
 Those are scripts used for the specific analysis tasks that have evolved in quality over time and are not complete. Probably require adaption for any other use.
+
+## Citation
+
+The code was developed during my PhD project for the analysis of free energy evolution and pore network filling during wicking in yarns.
+If you find any parts of this repository useful for your work, please consider citing one of these publications (might me also interesting reads on their own ;)
+
+```bibtex
+@article{Fischer2021,
+   author = {Fischer, Robert and Schlepütz, Christian M. and Hegemann, Dirk and Rossi, René M. and Derome, Dominique and Carmeliet, Jan},
+   title = {Four-dimensional imaging and free-energy analysis of sudden pore-filling events in wicking of yarns},
+   journal = {Physical Review E},
+   volume = {103},
+   number = {5},
+   pages = {053101},
+   DOI = {10.1103/PhysRevE.103.053101},
+   year = {2021},
+   type = {Journal Article}
+}
+
+```
+
+```bibtex
+@article{Fischer2022,
+   author = {Fischer, Robert and Schlepütz, Christian M. and Zhao, Jianlin and Boillat, Pierre and Hegemann, Dirk and Rossi, René M. and Derome, Dominique and Carmeliet, Jan},
+   title = {Wicking dynamics in yarns},
+   journal = {Journal of Colloid and Interface Science},
+   volume = {625},
+   pages = {1-11},
+   DOI = {10.1016/j.jcis.2022.04.060},
+   year = {2022},
+   type = {Journal Article}
+}
+```bibtex
+@article{Fischer2022a,
+   author = {Fischer, R. and Schleputz, C. M. and Rossi, R. M. and Derome, D. and Carmeliet, J.},
+   title = {Wicking through complex interfaces at interlacing yarns},
+   journal = {J Colloid Interface Sci},
+   volume = {626},
+   pages = {416-425},
+   DOI = {10.1016/j.jcis.2022.06.103},
+   year = {2022},
+   type = {Journal Article}
+}
+
+```
--- processing_final_version/get_scan_time_stamps.py
+++ processing_final_version/get_scan_time_stamps.py
@@ -9,49 +9,81 @@ import h5py
 import os
 import numpy as np
 import json
-import pickle
+# import pickle
 
-baseFolder = r"/Volumes/Volume/disk1_2"
-targetFolder = r"/Users/robfisch/NAS"
+# baseFolder = r"/Volumes/Volume/disk1_2"
+# targetFolder = r"/Users/robfisch/NAS"
 
-samples = os.listdir(baseFolder)
+# samples = os.listdir(baseFolder)
 # samples = os.listdir(targetFolder)
 
 
-TIME = {}
+# TIME = {}
+# 
 
-for sample in samples:
+def get_scan_timestamps(sample, baseFolder, n0=1000):
     print(sample)
     sample_id = sample
-    if sample[-6:-2] == 'yarn':
-        sample_id = sample[:-7]
-    metapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'_config.json']))
+    # metapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'_config.json'])) #old filename for interlaces
+    metapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'.json'])) #"new" naming for COELY
     datapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'.h5']))
     
     if  os.path.exists(metapath):
         metadata = json.load(open(metapath,'r'))
-        n = metadata['nimages']
+        # n = metadata['nimages']
+        n = metadata['scientificMetadata']['scanParameters']['Number of projections'] #new json convention
         
     else:
-        print('config file not found, hard coded n=500')
-        n = 500
+        print('config file not found, hard coded n='+str(n0))
+        n = n0
 
     if not os.path.exists(datapath):
         print('h5 file not found')
-        continue
+        return None
+    else:
+        datafile = h5py.File(datapath, 'r')
+        data = datafile['measurement']['instrument']['acquisition']['data']
+        data = np.array(data)
+        time_stamps = np.zeros(data.size)
+        
+        for i in range(data.size):
+            time_stamps[i] = data[i][-2]
+            t0 = data[i][-3]
+            
+        return time_stamps[::n]/1e7, t0
+
+# for sample in samples:
+#     print(sample)
+#     sample_id = sample
+#     if sample[-6:-2] == 'yarn':
+#         sample_id = sample[:-7]
+#     metapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'_config.json']))
+#     datapath = os.path.join(baseFolder, sample_id, ''.join([sample_id,'.h5']))
+    
+#     if  os.path.exists(metapath):
+#         metadata = json.load(open(metapath,'r'))
+#         n = metadata['nimages']
+        
+#     else:
+#         print('config file not found, hard coded n=500')
+#         n = 500
+
+#     if not os.path.exists(datapath):
+#         print('h5 file not found')
+#         continue
     
     
-    datafile = h5py.File(datapath, 'r')
-    data = datafile['measurement']['instrument']['acquisition']['data']
+#     datafile = h5py.File(datapath, 'r')
+#     data = datafile['measurement']['instrument']['acquisition']['data']
       
     
-    data = np.array(data)
-    time_stamps = np.zeros(data.size)
+#     data = np.array(data)
+#     time_stamps = np.zeros(data.size)
     
-    for i in range(data.size):
-        time_stamps[i] = data[i][-2]
+#     for i in range(data.size):
+#         time_stamps[i] = data[i][-2]
         
-    TIME[sample] = time_stamps[::n]/1e7
+#     TIME[sample] = time_stamps[::n]/1e7
 
-dumpfile = os.path.join(targetFolder, 'T2_time_part_1.p')
-pickle.dump(TIME, open(dumpfile, 'wb'))
+# dumpfile = os.path.join(targetFolder, 'T2_time_part_1.p')
+# pickle.dump(TIME, open(dumpfile, 'wb'))
